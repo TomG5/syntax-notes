5b149064eb58040d4242769e21fa5d1c10eac7e7
TomG5 <86327165+TomG5@users.noreply.github.com>
2021-08-04 01:48:04 -0300

    Add files via upload

--- syntaxpython.py
+++ syntaxpython.py
@@ -2,18 +2,18 @@
 
 ##  Tipos de Dados:
 '''
-str     : String    # Cadeia de Caracteres
-int     : Integer   # Número Inteiro
-float   : Float     # Número Decimal
-bool    : Boolean   # Verdadeiro ou Falso
+str     |   String    # Cadeia de Caracteres
+int     |   Integer   # Número Inteiro
+float   |   Float     # Número Decimal
+bool    |   Boolean   # Verdadeiro ou Falso
 '''
 
 ##  Estruturas de Dados:
 '''
-[A, B, C]           : List          # Aceita valores únicos, cadeias de valores, outras variáveis etc.
-{'Blue': 'Azul'}    : Dictionary    # Formato em pares de < Chave: Valor >
-{Jan, Fev, Mar}     : Set           # Inalterável após definido, mas pode receber novas entradas / Somente valores únicos
-("Operação X")      : Tuple         # Inalterável após definido
+[A, B, C]           |   List          # Aceita valores únicos, cadeias de valores, outras variáveis etc.
+{'Blue': 'Azul'}    |   Dictionary    # Formato em pares de < Chave: Valor >
+{Jan, Fev, Mar}     |   Set           # Valores já adicionados não podem ser alterados, mas pode receber novas entradas / Somente valores únicos
+("Operação X")      |   Tuple         # Inalterável após definido
 '''
 
 ##  Definição de Variáveis:
@@ -42,46 +42,49 @@ def somar_valores(valor1, valor2):
 
 ##  Funções Padrões:
 '''
-abs()       : remover sinal numérico
-all()       : retorna verdadeiro se todo elemento iterado for True
-any()       : retorna verdadeiro se qualquer elemento iterado for True
-bin()       : retorna valor binário de número inteiro
-chr()       : retorna o caracter de um índice do padrão Unicode
-dict()      : transforma variável em dicionário
-drop()      : remove índice, coluna ou valores de uma matriz (colocando 'inplace:True' como parâmetro efetiva a mudança)
-filter()    : filtra registros cujo resultado da função seja True # Sintaxe < filter(*** função ***, *** iteravel ***) >
-format()    : inclui valores de variáveis em strings delimitadas por {} (utilizado após a string)
-frozenset() : transforma variável em set imutável
-hash()      : gera um hash do valor
-head()      : printar primeiros resultados
-hex()       : retorna valor hexadecimal de número inteiro
-index()     : retorna o índice de um dado na variável
-input()     : recebe entrada de dados pelo usuário
-list()      : transforma variável em lista
-max()       : máximo de múltiplos dados
-min()       : mínimo de múltiplos dados
-ord()       : retorna o número de um caracter no padrão Unicode
-round()     : arredonda valores conforme número de casas decimais delimitadas
-set()       : transforma variável em set (esta função opera de forma iteradora, segregando os elementos a serem adicionados)
-sum()       : soma de valores
-tail()      : printar últimos resultados
-tuple()     : transforma variável em tupla
-type()      : retorna o tipo de dado da variável
-unique()    : printar valores únicos de dada coluna, lista etc.
+abs()       |   remover sinal numérico
+all()       |   retorna verdadeiro se todo elemento iterado for True
+any()       |   retorna verdadeiro se qualquer elemento iterado for True
+bin()       |   retorna valor binário de número inteiro
+chr()       |   retorna o caracter de um índice do padrão Unicode
+dict()      |   transforma variável em dicionário
+drop()      |   remove índice, coluna ou valores de uma matriz (adicionando parâmetro 'inplace=True' efetiva a mudança)
+filter()    |   filtra registros cujo resultado da função seja True #Sintaxe < filter(*** função ***, *** iteravel ***) >
+format()    |   inclui valores de variáveis em strings delimitadas por {} (utilizado após a string)
+frozenset() |   transforma variável em set imutável
+hash()      |   gera um hash do valor
+head()      |   printar primeiros resultados
+hex()       |   retorna valor hexadecimal de número inteiro
+index()     |   retorna o índice de um dado no objeto
+input()     |   recebe entrada de dados pelo usuário
+len()       |   conta quantos elementos tem no objeto
+list()      |   transforma variável em lista
+max()       |   máximo de múltiplos dados
+min()       |   mínimo de múltiplos dados
+ord()       |   retorna o número de um caracter no padrão Unicode
+range()     |   retorna elementos de um dado intervalo (inclusivo, não inclusivo)
+round()     |   arredonda valores conforme número de casas decimais delimitadas
+set()       |   transforma variável em set (esta função opera de forma iteradora, segregando os elementos a serem adicionados)
+sum()       |   soma de valores
+tail()      |   printar últimos resultados
+tuple()     |   transforma variável em tupla
+type()      |   retorna o tipo de dado da variável
+unique()    |   printar valores únicos de dada coluna, lista etc.
+zip()       |   integra múltiplos objetos iteráveis com base nos índices de seus elementos
 '''
 
 ##  Operadores
 '''
-not : Negação
-in  : Em
-and : E
-or  : Ou
-==  : Igualdade
-!=  : Desigualdade
->=  : Maior ou igual
-<=  : Menor ou igual
-%   : Módulo
-**  : Exponenciação
+not     |   Negação
+in      |   Em
+and     |   E
+or      |   Ou
+==      |   Igualdade
+!=      |   Desigualdade
+>=      |   Maior ou igual
+<=      |   Menor ou igual
+%       |   Módulo
+**      |   Exponenciação
 '''
 
 ##  Condicionais
@@ -139,13 +142,13 @@ close(file)
 
     ## Modos:
 
-'r' : aberto para leitura
-'w' : aberto para escrita, truncando o arquivo primeiro
-'x' : aberto exclusivamente para criação, falhando se o arquivo já existir
-'a' : aberto para escrita, agregando ao final do arquivo caso exista
-'b' : modo binário
-'t' : modo textual
-'+' : aberto para atualizar (leitura e escrita)
+'r'     |   aberto para leitura
+'w'     |   aberto para escrita, truncando o arquivo primeiro
+'x'     |   aberto exclusivamente para criação, falhando se o arquivo já existir
+'a'     |   aberto para escrita, agregando ao final do arquivo caso exista
+'b'     |   modo binário
+'t'     |   modo textual
+'+'     |   aberto para atualizar (leitura e escrita)
 
     ## Anotações:
 
@@ -156,28 +159,27 @@ Para manipular arquivos multimídia utilizar o modo binário: rb, wb, ab
 
 ##  Manipulação de Strings
 '''
-    ## Funções:
+    ## Método  |   Descrição    |   Sintaxe:
 
-upper()       : todas as letras maiúsculas
-lower()       : todas as letras minúsculas
-isupper()     : valida letras maiúsculas
-islower()     : valida letras minúsculas
-title()       : todas as primeiras letras das palavras maiúsculas
-strip()       : remover espaços
-join()        : juntar palavras # Sintaxe < *** separador ***.join(lista) >
-split()       : separar palavras da mesma string
-replace()     : substituir caracter
-find()        : retorna o índice do string procurado na palavra
-capitalize()  : mantém apenas a primeira letra da string em maiúsculo
-reverse()     : reverte a ordem da lista
+upper()       |     todas as letras maiúsculas                              |   my_string.upper()
+lower()       |     todas as letras minúsculas                              |   my_string.lower()
+isupper()     |     valida se são letras maiúsculas                         |   my_string.isupper()
+islower()     |     valida se são letras minúsculas                         |   my_string.islower()
+title()       |     todas as primeiras letras das palavras maiúsculas       |   my_string.title()
+strip()       |     remover espaços                                         |   my_string.strip()
+join()        |     juntar elementos                                        |   "separador".join(letras)
+split()       |     separar palavras da mesma string                        |   my_string.split("separador")
+replace()     |     substituir caracter                                     |   my_string.replace("antigo", "novo")
+find()        |     retorna o índice do caracter procurado na palavra       |   my_string.find("caracter")
+capitalize()  |     mantém apenas a primeira letra da string em maiúsculo   |   my_string.capitalize()
 
     ## Slicers:
 
-var[0:3]    : pegar os dígitos das posições 0, 1 e 2 da string
-var[:3]     : pegar os três primeiros dígitos da string
-var[3:]     : pegar do índice 3 ao último da string
-var[-3]     : pegar os últimos três dígitos da string
-var[:]      : pegar todos os dígitos
+var[0:3]    |   pegar os dígitos das posições 0, 1 e 2 da string
+var[:3]     |   pegar os três primeiros dígitos da string
+var[3:]     |   pegar do índice 3 ao último da string
+var[-3]     |   pegar os últimos três dígitos da string
+var[:]      |   pegar todos os dígitos
 
     ## Escape Sequences:
 
@@ -193,36 +195,42 @@ var[:]      : pegar todos os dígitos
 
 my_list[*** índice ***]
 
-    ## Funções:
+    ## Alteração de Elemento:
 
-append()      : adiciona um valor em lista
-extend()      : adiciona uma lista dentro de outra
-insert()      : adiciona dado em lista em dada posição
-remove()      : remover item de lista por valor
-pop()         : remover item de lista por índice (caso em branco remove o último)
-del()         : remover item de lista por índice ou slice
-clear()       : limpa toda a lista
-sort()        : ordena itens de uma lista (uso após a variável)
-sorted()      : ordena itens de uma lista (uso sobre a variável)
-zip()         : integra múltiplas listas com base nos índices de seus elementos
-count()       : conta a frequência de dado elemento numa lista
-len()         : conta quantos elementos tem numa lista
-index()       : retorna índice de item em lista
-reverse()     : reverte a ordem da lista
+my_list[índice] = *** valor novo ***
+
+    ## Método  |   Descrição    |   Sintaxe:
+
+append()      |     adiciona um valor ao final da lista                                 |   my_list.append(valor)
+extend()      |     adiciona valores de uma lista dentro de outra na sequência          |   my_list.extend(outra_lista)
+insert()      |     adiciona dado em lista em dada posição                              |   my_list.insert(índice, valor)
+remove()      |     remover item de lista por valor                                     |   my_list.remove(valor)
+pop()         |     remover item de lista por índice (caso em branco remove o último)   |   my_list.pop(índice)
+clear()       |     limpa toda a lista                                                  |   my_list.clear()
+sort()        |     ordena itens de uma lista                                           |   my_list.sort()
+count()       |     conta a frequência de dado elemento numa lista                      |   my_list.count(valor)
+index()       |     retorna índice de item em lista                                     |   my_list.index(valor)
+reverse()     |     reverte a ordem da lista                                            |   my_list.reverse()
+
+del           |     remover item de lista por índice ou slice                                   |   del my_list[índice]
+sorted()      |     ordena itens de uma lista                                                   |   sorted(my_list)
+zip()         |     integra múltiplos objetos iteráveis com base nos índices de seus elementos  |   my_list = list(zip(obj1, obj2))
 
     ## Slicers:
 
-var[0:3]    : pegar os elementos das posições 0, 1 e 2
-var[:3]     : pegar os três primeiros elementos
-var[3:]     : pegar do índice 3 ao último da lista
-var[-3]     : pegar os últimos três elementos
-var[:]      : pegar todos os elementos
+var[0:3]    |   pegar os elementos das posições 0, 1 e 2
+var[:3]     |   pegar os três primeiros elementos
+var[3:]     |   pegar do índice 3 ao último da lista
+var[-3]     |   pegar os últimos três elementos
+var[:]      |   pegar todos os elementos
 
     ## List Comprehensions (Criação de listas a partir de objetos iteráveis):
 
-new_list = [x for x in some_iterable]
+new_list = [x for x in objeto_iterável]
 
-new_list = [x for x in some_iterable if *** condição ***]
+new_list = [x for x in objeto_iterável if *** condição ***]
+
+new_list = [x if *** condição *** for x in objeto_iterável]
 
 old_list = [8, 13, -7, 4, -9, 2, 10]
 new_list = [num if num >= 0 else 0 for num in old_list]
@@ -232,61 +240,82 @@ new_list = [num if num >= 0 else 0 for num in old_list]
 '''
     ## Chamada:
 
-my_dict[*** Chave ***] 
+my_dict[*** chave ***]
 
-    ## Funções:
+    ## Alteração de Elemento:
 
-update()  : adiciona/atualiza dados a um dicionário
-pop()     : remove item do dicionário
-del()     : remove item do dicionário
-clear()   : limpa todo o dicionário
-zip()     : integra duas listas em um dicionário
-get()     : procura dada chave e retorna valor booleano
-keys()    : retorna chaves do dicionário
-values()  : retorna valores do dicionário
-items()   : retorna conjuntos de chaves e valores do dicionário (transformar em lista para pesquisar por índice)
+my_dict[chave] = *** valor novo ***
+
+    ## Método  |   Descrição    |   Sintaxe:
+
+update()  | atualiza ou adiciona dados a um dicionário conforme chave   |   my_dict.update({chave: valor}) ou my_dict.update(chave = valor)
+pop()     | remove item do dicionário                                   |   my_dict.pop(chave)
+clear()   | limpa todo o dicionário                                     |   my_dict.clear()
+get()     | procura dada chave e retorna valor                          |   my_dict.get(chave)
+keys()    | retorna chaves do dicionário                                |   my_dict.keys()
+values()  | retorna valores do dicionário                               |   my_dict.values()
+items()   | retorna conjuntos de chaves e valores do dicionário         |   my_dict.items()
+
+zip()     | integra múltiplos objetos iteráveis com base nos índices de seus elementos  |   my_dict = dict(zip(chaves, valores))
+
+* Para pesquisar por índice transforme o dicionário em lista
 
     ## Dictionary Comprehensions (Criação de dicionários a partir de objetos iteráveis):
 
-my_dict = {key: value if *** condição *** for element in some_iterable}
+my_dict = {chave: valor for x in objeto_iterável}
+
+my_dict = {chave: valor for x in objeto_iterável if *** condição ***}
+
+my_dict = {key: value if *** condição *** for element in objeto_iterável}
+
+my_dict = {chave: valor if *** condição *** else valor for x in objeto_iterável}
 '''
 
 ##  Manipulação de Sets
 '''
     ## Chamada:
 
-my_set[index]
+my_set[*** índice ***]
 
-    ## Funções:
+    ## Método  |   Descrição    |   Sintaxe:
 
-add()       : adiciona novos valores
-update()    : adiciona novos valores
-len()       : retorna tamanho do set
-remove()    : remove elemento do set
-clear()     : limpa os elementos do set
+add()       |   adiciona novos valores      |   my_set.add(valor)
+update()    |   adiciona um set a outro     |   my_set.update(outro_set)
+clear()     |   limpa os elementos do set   |   my_set.clear()
+
+zip()       |   integra múltiplos objetos iteráveis com base nos índices de seus elementos      |   my_set = set(zip(obj1, obj2))
 
     ## Operadores:
 
-|   : união
-&   : interseção
-–   : diferença             # Elementos de A que não existem em B e C
-^   : diferença simétrica   # Elementos que não existem em A e B, A e C ou B e C
+|   :   união
+&   :   interseção
+–   :   diferença             # Elementos de A que não existem em B e C
+^   :   diferença simétrica   # Elementos que não existem em A e B, A e C ou B e C
 
     ## Set Comprehensions (Criação de conjuntos a partir de objetos iteráveis):
 
-new_set = [x for x in some_iterable]
+new_set = {x for x in objeto_iterável}
 
-new_set = [x for x in some_iterable if *** condição ***]
+new_set = {x for x in objeto_iterável if *** condição ***}
 
-old_set = [8, 13, -7, 4, -9, 2, 10]
-new_set = [num if num >= 0 else 0 for num in old_set]
+new_set = {x if *** condição *** for x in objeto_iterável}
+
+old_set = {8, 13, -7, 4, -9, 2, 10}
+new_set = {num if num >= 0 else 0 for num in old_set}
 '''
 
 ## Manipulação de Tuplas
 '''
-tupla = (A1, B1, C1, D1)
+    ## Chamada:
 
-tupla[2] = retorna o valor de índice 2 na tupla
+my_tuple[*** índice ***]
+
+    ## Método  |   Descrição    |   Sintaxe:
+
+index() |   retorna o índice de um valor da tupla                   |   my_tuple.index(valor)
+count() |   retorna a contagem da ocorrência de um valor na tupla   |   my_tuple.count(valor)
+
+zip()   |   integra múltiplos objetos iteráveis com base nos índices de seus elementos  |   my_tuple = tuple(zip(obj1, obj2))
 '''
 
 D) Alguns Módulos:
@@ -313,6 +342,7 @@ datetime.now -> retorna a datação atual
 
 D5) random = randomizar
 random.randint -> gerar número aleatório dentro de um range / random.choice -> escolhe um dado de uma determinada lista / random.seed -> gera números aleatórios com base num código de referência (Ex: 101)
+random.shuffle
 
 D6) pickle = salvar cópia do script em determinado estado de execução
 *variável = open(file_Name,'rb') -> para ler / *variável = open(file_Name,'wb') -> para editar / *variável = pickle.load(arquivo) -> para passar dados para nova variável 
